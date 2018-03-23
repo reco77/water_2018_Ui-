0e369aa2ffa1375378e85ae1da93bd8c6409eaa4
Yasser Alzuhair <yasser@MacBook-Pro-alkhas-b-Yasser.local>
2018-03-23 20:43:41 +0300

    update dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from '@pages/home/home.component';
 import { DashboardComponent } from '@pages/dashboard/dashboard.component';
 import { appRoute } from '@app/app.route';
+import { CardChartComponent } from '@shared/chart-card/chart-card.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    CardChartComponent
   ],
   imports: [
     BrowserModule,
